a8b8fb576ac54dfdd14832868c56727559cc4ebb
Arshad101 <ansarimdarshadmd@gmail.com>
2024-03-01 14:31:33 +0530

    vvv

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome bbbdsaa to <a href="https://nextjs.org">Next.js!</a>
+          Welcome bbbdsadsdsdsda to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
